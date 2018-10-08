6a366e146d564d6dee1dde7dda444082b5ca5fe1
Rafal Leszko <rafal@hazelcast.com>
2018-10-08 15:55:48 +0200

    New version of hazelcast-enterprise

---
+++ update.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+helm package ~/rafal/Hazelcast/repos/ibm-charts/community/hazelcast-enterprise
+helm repo index --url https://leszko.github.io/hazelcast-charts/ .
+git add .
+git commit -m "New version of hazelcast-enterprise" --no-edit
+git push origin master
